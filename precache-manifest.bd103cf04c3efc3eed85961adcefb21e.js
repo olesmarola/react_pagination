f427c7a10a8b3467c4a1f8b2f6f76e433cd9f44a
Oles Marola <oles_pw@ukr.net>
2019-12-16 19:22:52 +0200

    Updates

---
+++ precache-manifest.bd103cf04c3efc3eed85961adcefb21e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fade58a27dcb3c76434fcb05652bf013",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "8f8aaaa35f026f3fcd1b",
+    "url": "/react_pagination/static/css/main.f50dccf0.chunk.css"
+  },
+  {
+    "revision": "ac1cbc47369b80f4fac9",
+    "url": "/react_pagination/static/js/2.fbcb28bf.chunk.js"
+  },
+  {
+    "revision": "8f8aaaa35f026f3fcd1b",
+    "url": "/react_pagination/static/js/main.1e33e2f6.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
